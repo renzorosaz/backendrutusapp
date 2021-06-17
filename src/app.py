97a8016aa26cc25df49d65b2b34594a3d291844e
renzo <renzo.rovi@gmail.com>
2021-06-17 18:21:51 -0500

    first commit

---
+++ src/app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://root:030115lol@localhost/flaskmysql'
+app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
+
+
+db = SQLAlchemy(app)
+ma= Marshmallow(app)
